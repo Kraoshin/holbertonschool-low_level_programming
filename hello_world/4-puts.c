2e0d0a3c89203c2383646e9b950e81bb0ac07359
kraoshin <10072@holbertonstudents.com>
2024-10-21 11:22:06 +0200

    program that print string

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - fonction
+ * str - array to print
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char str[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(str);
+
+	return (0);
+}
