b23cbc3c52f5fe179f8826cec7b65438d0887e06
kraoshin <10072@holbertonstudents.com>
2024-10-22 10:40:49 +0200

    program that prints alphabet from ascii

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		putchar (n);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
