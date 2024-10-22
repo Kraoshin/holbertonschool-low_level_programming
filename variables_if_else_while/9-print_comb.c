4921390eeb9bd1b1c575ca8e42c2115b00867b2d
kraoshin <10072@holbertonstudents.com>
2024-10-22 12:12:26 +0200

    script that display all single digit followed by char after all of them but the last

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar (n);
+		if (n < 57)
+		{
+		putchar (44);
+		putchar (32);
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
