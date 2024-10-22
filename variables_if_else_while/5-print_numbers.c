cf5b013d251cf32ffb778089d03e7354e48c9b68
kraoshin <10072@holbertonstudents.com>
2024-10-22 11:26:46 +0200

    add 2 program that display number between 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+
+	return (0);
+}
