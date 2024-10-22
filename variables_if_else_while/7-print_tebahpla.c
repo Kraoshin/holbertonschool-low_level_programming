d87d064f0ddf77bc05c762eee429c18ebe3e4efc
kraoshin <10072@holbertonstudents.com>
2024-10-22 11:30:10 +0200

    program that display alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+	{
+		putchar (n);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
