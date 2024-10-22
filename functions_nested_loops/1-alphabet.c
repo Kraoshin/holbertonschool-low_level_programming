5eec02923ea19d22422d63b0d26143413a68a47c
kraoshin <10072@holbertonstudents.com>
2024-10-22 18:40:02 +0200

    program that display alphabet from a fonction created myself

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in low cases
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		_putchar (n);
+	}
+
+	_putchar ('\n');
+}
