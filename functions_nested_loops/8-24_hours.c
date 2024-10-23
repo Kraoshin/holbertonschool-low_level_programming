545afcf0bbbc04065cbc1330a8733effba38e6ec
kraoshin <10072@holbertonstudents.com>
2024-10-23 21:56:50 +0200

    function that display every minutes in an hour for each hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,48 +6,19 @@
 
 void jack_bauer(void)
 {
+	int h;
 	int m;
-	int h; /* 1 de 0 a 59 l'autre de 0 a 23*/
-	m = 48;
-	h = 48;
 
-	while (dh < 51  && h < 52  && dm < 54  && m < 58 )
+	for (h = 0; h < 24; h++)
 	{
-
-		for (m = 48; m < 58; m++)
+		for (m = 0; m < 60; m++)
 		{
-			_putchar(dh);
-			_putchar(h);
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(58);
-			_putchar(dm);
-			_putchar(m);
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
-
-			/*if (h == 51 && dh == 50 && dm == 53 && m == 57)
-			{
-				break;
-			}*/
-
-			if (m == 58)
-			{
-				dm++;
-				m = 48;
-			/*	if (h == 51 && dh == 50)
-				{
-					continue;
-				}*/
-				if (dm == 53)
-				{
-					h++;
-					dm = 48;
-
-					if (h == 51)
-					{
-						dh++;
-						h = 0;
-					}
-				}
-			}
 		}
 	}
 }
