545fbf478a9dd1a7e275d940caa015d75c08899b
kraoshin <10072@holbertonstudents.com>
2024-10-24 12:18:22 +0200

    function that print all the 9 time table as a table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,47 +1,36 @@
 #include "main.h"
 
 /**
- * times_table _ print the 9 times tables starting with 0
+ * times_table - print the 9 times tables starting with 0
  */
 
 void times_table(void)
 {
-	int a;
-	int b;
+	int a, b, r;
+	int tens, units;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			int r = (a * b);
+			r = (a * b);
+			tens = r / 10;
+			units = r % 10;
 
-			if (r >= 10 && b != 9)
+			if (r >= 10)
+				_putchar(tens + '0');
+			else if (b > 0)
+				_putchar(' ');
+
+			_putchar(units + '0');
+
+			if (b < 9)
 			{
-				_putchar((r / 10) + '0');
-				_putchar((r % 10) + '0');
-				_putchar(44);
-				_putchar(32);
+				_putchar (',');
+				_putchar(' ');
 			}
 
-			if (r < 10 && b != 9)
-			{
-				_putchar(r + '0');
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-			}
-
-			if (b == 9 && r >= 10)
-			{
-				_putchar((r / 10) + '0');
-				_putchar((r % 10) + '0');
-				_putchar('\n');
-			}
-			if (b == 9 && r < 10)
-			{
-				_putchar(r + '0');
-				_putchar('\n');
-			}
 		}
+		_putchar ('\n');
 	}
 }
