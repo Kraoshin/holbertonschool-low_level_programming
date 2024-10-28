7289814a2230aa4c79a82a8b1f9f96bb7b7e8783
kraoshin <10072@holbertonstudents.com>
2024-10-28 11:59:32 +0100

    program that answer to the fizz buzz test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,16 @@ int main(void)
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
 
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz ");
 
-		if (i % 5 == 0)
+		if (i % 5 == 0 && i % 3 != 0 && i != 100)
 			printf("Buzz ");
 
 		if ((i % 3 != 0) && (i % 5 != 0))
 			printf("%d ", i);
+		if (i == 100 && (i % 5 == 0))
+			printf("Buzz");
 	}
 	printf("\n");
 
