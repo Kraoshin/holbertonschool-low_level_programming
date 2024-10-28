ef9cfd78c9bf2e23cb743a9f57dc33b3f349766c
kraoshin <10072@holbertonstudents.com>
2024-10-28 11:18:39 +0100

    function that prints a square, followed by a new line.

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - function that draws a straight line in the terminal.
+ * @n: the number of '_' to be printed
+ */
+
+void print_square(int size)
+{
+	int boucle;
+	int space;
+
+	if (size <= 0)
+		_putchar('\n');
+
+
+	else
+		for (boucle = 0; boucle < size; boucle++)
+		{
+			for (space = 1; space < size; space++)
+				_putchar('#');
+
+			_putchar('#');
+			_putchar('\n');
+		}
+}
