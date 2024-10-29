fc9150b5230633c23052d3544972f1c9a79142d7
kraoshin <10072@holbertonstudents.com>
2024-10-29 10:59:06 +0100

    function that reverses a string inside an array.

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,25 @@
 #include "main.h"
 
 /**
- *
+ * rev_string - reverse the char inside an array
+ * @s: the array to reverse
  */
 
 void rev_string(char *s)
 {
+	int length = 0, a = 0, b, temp = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	b = length - 1;
+
+	while (a < length / 2)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+		a++;
+		b--;
+	}
 }
