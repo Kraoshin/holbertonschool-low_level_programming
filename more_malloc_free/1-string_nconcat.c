72881e873fc3129164e11bdee90ff3324d2c1921
kraoshin <10072@holbertonstudents.com>
2024-11-13 09:04:32 +0100

    function that concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,87 +2,42 @@
 #include <stdlib.h>
 
 /**
- * _strlen - the strlen function rewrite
- * @str: the str we want to count the len
+ * string_nconcat - allocate space for concatenate 2 str
+ * @s1: begining of the str
+ * @s2: follows what's written in s1
+ * @n: number of bytes from s2 to be concatenate
  *
- * Return: the length of the str
+ * Return: NULL if not enough space, the allocated mem otherwise
  */
 
-unsigned int _strlen(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = 0;
-
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
-/**
- * _malloc - allocate space for s1 + n bytes
- * @s1: the dest
- * @s2: the src
- * @n: number of bytes we want from s2
- *
- * Return: if empty or not enough space NULL otherwise the allocate mem
- */
-
-char *_malloc(char *s1, char *s2, unsigned int n)
-{
-	unsigned int len1, len2;
-	char *mem;
+	char *concat;
+	unsigned int i = 0, j = 0, k = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
 
-	if (n >= len2)
-		n = len2;
-	mem = malloc(sizeof(char) * len1 + n + 1);
+	while (s1[i] != '\0')
+		i++;
 
-	if (mem == NULL)
+	while (s2[j] != '\0' && j < n)
+		j++;
+
+	concat = malloc(i + j + 1);
+
+	if (concat == NULL)
 		return (NULL);
-	return (mem);
-}
 
-/**
- * string_nconcat - concat n s2 bytes in s1
- * @s1: the dest
- * @s2: the src
- * @n: the number of bytes from s2 to be concatenate
- *
- * Return: if empty or not enough space NULL otherwise the concatenate str
- */
+	for ( ; k < i; k++)
+		concat[k] = s1[k];
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	unsigned int i, len1, len2;
-	char *mem, *temp;
+	for ( ; k < j; k++)
+		concat[i + k] = s2[k];
 
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	concat[i + j] = '\0';
 
-	if (n >= len2)
-		n = len2;
-
-	mem = _malloc(s1, s2, n);
-
-	temp = mem;
-
-	for (i = 0; i < len1; i++)
-	{
-		*temp = s1[i];
-		temp++;
-	}
-	for (i = 0; i < n; i++)
-	{
-		*temp = s2[i];
-		temp++;
-	}
-
-	*temp = '\0';
-
-	return (mem);
+	return (concat);
 }
