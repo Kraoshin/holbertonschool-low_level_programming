8541dcd22788cfc4df4171ac8a89ccc71d38a020
kraoshin <10072@holbertonstudents.com>
2024-11-13 15:56:34 +0100

    function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -31,7 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (i = 0; i < len1)
+	for (i = 0; i < len1; i++)
 		dog->name[i] = name[i];
 	dog->name[len1] = '\0';
 
