55ee9760ac7c45a65a8aa92a274a40a5c2c69fcb
kraoshin <10072@holbertonstudents.com>
2024-11-18 16:26:08 +0100

    print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
-
 /**
- * print_name - print name depending on which function is passed as argument
- * @name: the name we want to display
- * @f: the function the name is passed by
+ * print_name - pass a name to a function to print it
+ * @name: char pointer - name
+ * @f: function pointer
+ *
+ * Return: void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
