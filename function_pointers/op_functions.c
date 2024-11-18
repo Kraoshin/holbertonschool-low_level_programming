4c169d93c47c380ab37999b6f0c28273c55d0bad
kraoshin <10072@holbertonstudents.com>
2024-11-18 11:22:41 +0100

    add the c file that contains all calculations

---
+++ function_pointers/op_functions.c
@@ -0,0 +1,66 @@
+#include "calc.h"
+
+/**
+ * op_add - add numbers
+ * @a: first num
+ * @b: second num
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract numbers
+ * @a: first num
+ * @b: second num
+ *
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply numbers
+ * @a: first num
+ * @b: second num
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div numbers
+ * @a: first num
+ * @b: second num
+ *
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod numbers
+ * @a: first num
+ * @b: second num
+ *
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
