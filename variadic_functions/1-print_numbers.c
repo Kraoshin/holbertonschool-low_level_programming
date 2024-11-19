b382d76dd55f76d6b3c1bc180ea2a357b8dfa86b
kraoshin <10072@holbertonstudents.com>
2024-11-19 15:14:45 +0100

    correct file 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,22 +9,26 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	int num;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		int num = va_arg(args, int);
+		num = va_arg(args, int);
 
-		if (i == 0)
+		if (!separator)
+			printf("%d", num)
+
+		else if (separator && i == 0)
 			printf("%d", num);
 
 		else
-			printf("%s%d", separator ? separator : "", num);
+			printf("%s%d", separator, num);
 	}
 
-	va_end(args);
-
 	printf("\n");
+
+	va_end(args);
 }
