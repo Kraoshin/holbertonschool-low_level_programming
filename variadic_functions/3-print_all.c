fe5c84fad7e37c76127553f6f48ba43a06f3bcd1
kraoshin <10072@holbertonstudents.com>
2024-11-19 15:51:38 +0100

    modified alias struct name

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,66 +1,89 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - print char
+ * @args: list of args
+ */
+
+void print_char(va_list args)
+{
+        printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - print char
+ * @args: list of args
+ */
+
+void print_int(va_list args)
+{
+        printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - print char
+ * @args: list of args
+ */
+
+void print_float(va_list args)
+{
+        printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - print char
+ * @args: list of args
+ */
+
+void print_string(va_list args)
+{
+        char *x = va_arg(args, char*);
+
+        if (x == NULL)
+{
+                printf("(nil)");
+                return;
+        }
+        printf("%s", x);
+}
+
+/**
  * print_all - print all type of arg passed in the variadic function
  * @format: the list of types of arg
  */
 
 void print_all(const char * const format, ...)
 {
-<<<<<<< HEAD
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
-=======
-	va_list args;
-	char *separator = "";
-	int i = 0, j = 0;
+        va_list args;
+        char *separator = "";
+        int i = 0, j = 0;
 
-	get_op var[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string},
-		{NULL, NULL}
-	};
+        id_t var[] = {
+                {"c", print_char},
+                {"i", print_int},
+                {"f", print_float},
+                {"s", print_string},
+                {NULL, NULL}
+        };
 
-	va_start(args, format);
->>>>>>> 5e59c14 (change alias name of the struct)
+        va_start(args, format);
 
-	va_start(valist, format);
-	while (format && format[i])
-	{
-		j = 0;
-		while (t_arg[j])
-		{
-			if (format[i] == t_arg[j] && c)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
-				break;
-			}
-			printf("%s", str);
-			break;
-		} i++;
-	}
-	printf("\n"), va_end(valist);
+        while (format && format[i])
+        {
+                while (var[j].id)
+                {
+                        if (*var[j].id == format[i])
+                        {
+                                printf("%s", separator);
+                                var[j].f(args);
+                                separator = ", ";
+                        }
+                        j++;
+                }
+                j = 0;
+i++;
+        }
+        printf("\n");
+
+        va_end(args);
 }
