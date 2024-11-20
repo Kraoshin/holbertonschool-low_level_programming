c7f4b954a557b577d000a77c2d23d2777aa0a02f
kraoshin <10072@holbertonstudents.com>
2024-11-20 09:28:01 +0100

    add betty style to the struct

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,6 +9,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct op - struct containing pointer and func pointer
+ * @op: pointer pointing to a char
+ * @f: fonction pointer
+ *
+ * Structure that takes n element of char to compare with a char in a str
+ * and then execute the right function through the function pointer
+ */
+
 typedef struct op
 {
 	char *op;
