5cccdb1266c10ebae02abc820e2f8daba1020ea5
kraoshin <killian.kerleau@outlook.fr>
2024-12-03 10:00:42 +0100

    function that adds a new node at the end of a list

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,7 +31,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 
 	if (tmp != NULL)
-		lastn->next = new_node;
+		tmp->next = new_node;
 
 	if (*head == NULL)
 		*head = new_node;
